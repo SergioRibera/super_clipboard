ba86978fc6368fd141d0bb5ede83891161cb5456
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2023-03-15 09:59:10 -0400

    feat: main application

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+#![windows_subsystem = "windows"]
+
+mod args;
+mod daemon;
+mod settings;
+mod ui;
+
+use args::AppArgs;
+use clap::Parser;
+use iced::{Application, Settings};
+use preferences::AppInfo;
+use ui::MainApp;
+
+pub const APPINFO: AppInfo = AppInfo {
+    name: env!("CARGO_PKG_NAME"),
+    author: env!("CARGO_PKG_AUTHORS"),
+};
+
+pub const APP_WIDTH: i32 = 350;
+pub const APP_HEIGHT: i32 = 450;
+pub const APP_MOUSE_MARGIN: i32 = 25;
+
+fn main() -> iced::Result {
+    let _args = AppArgs::parse();
+
+    MainApp::run(Settings {
+        window: iced::window::Settings {
+            size: (APP_WIDTH as u32, APP_HEIGHT as u32),
+            visible: true,
+            resizable: false,
+            decorations: false,
+            transparent: true,
+            always_on_top: true,
+            ..Default::default()
+        },
+        ..Default::default()
+    })
 }
