970565bf497c224a8d4d014240652feb94f0fc9f
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2023-07-31 20:09:49 -0400

    fix: remove unnecesary comments

--- src/sync/mod.rs
+++ src/sync/mod.rs
@@ -45,10 +45,7 @@ pub fn create_socket() -> Arc<UdpSocket> {
     info!("Starting server on: {}", addr);
     info!("Multicast address: {}\n", multi_addr);
 
-    // let std_socket = bind_multicast(&addr, &multi_addr).expect("Failed to bind multicast socket");
     Arc::new(bind_multicast(&addr, &multi_addr).expect("Failed to bind multicast socket"))
-    // let socket = UdpSocket::from_std(std_socket).unwrap();
-    // Arc::new(socket)
 }
 
 pub fn start_sync() -> Subscription<Event> {
@@ -98,58 +95,6 @@ pub fn start_sync() -> Subscription<Event> {
             }
         },
     )
-
-    // match state {
-    //     State::Disconnected(app_sender, app_receiver) => {
-    //         let socket = create_socket();
-    //         let multi_addr = SocketAddrV4::new(
-    //             DEFAULT_MULTICAST.parse::<Ipv4Addr>().unwrap(),
-    //             DEFAULT_PORT,
-    //         );
-    //
-    //         let (sync_sender, sync_receiver) = mpsc::channel::<MDnsMessage>();
-    //
-    //         // receive from other devices
-    //         {
-    //             let socket = socket.clone();
-    //             std::thread::spawn(move || {
-    //                 let mut buff = [0u8; 4096];
-    //
-    //                 while let Ok(n) = socket.recv(&mut buff) {
-    //                     if n == 0 {
-    //                         continue;
-    //                     }
-    //                     let msg = MDnsMessage::from_bytes(&buff).unwrap();
-    //                     // send to application
-    //                     // app_sender.send(msg).unwrap();
-    //                     sync_sender.send(msg).unwrap();
-    //                 }
-    //             });
-    //         }
-    //
-    //         (
-    //             Some(Event::Connected),
-    //             State::Connected(multi_addr.into(), sync_receiver, socket),
-    //         )
-    //     }
-    //     // sender to other devices
-    //     // when app_conn receive data from application
-    //     // Receiver from application
-    //     State::Connected(addr, receiver, udp_tx) => match receiver.try_recv() {
-    //         Ok(msg) => {
-    //             let msg = msg.clone();
-    //             let udp_tx = udp_tx.clone();
-    //             let mut bytes = msg.to_bytes();
-    //             // send to other devices
-    //             udp_tx.send_to(&mut bytes[..], &addr).unwrap();
-    //             (
-    //                 Some(Event::Message(msg)),
-    //                 State::Connected(addr, receiver, udp_tx),
-    //             )
-    //         }
-    //         Err(_) => (None, State::Connected(addr, receiver, udp_tx)),
-    //     },
-    // }
 }
 
 #[derive(Debug)]
