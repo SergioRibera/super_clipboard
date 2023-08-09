46363c3035a1ae8c09bab9fed7be1e861f9ca3b8
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2023-08-09 00:37:19 -0400

    feat: define colors dark/light theme

--- mobile/android/src/com/sergioribera/superclipboard/ui/theme/Color.kt
+++ mobile/android/src/com/sergioribera/superclipboard/ui/theme/Color.kt
@@ -2,10 +2,40 @@ package com.sergioribera.superclipboard.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Brand Colors
+val Primary = Color(0xff4b39ef)
+val Secondary = Color(0xff39d2c0)
+val Tertiary = Color(0xffee8b60)
+val LightAlternate = Color(0xffe0e3e7)
+val DarkAlternate = Color(0xff262d34)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Utility Colors
+val LightPrimaryText = Color(0xff14181b)
+val LightSecondaryText = Color(0xff57636c)
+val LightPrimaryBackground = Color(0xfff1f4f8)
+val LightSecondaryBackground = Color(0xffffffff)
+val DarkPrimaryText = Color(0xffffffff)
+val DarkSecondaryText = Color(0xff95a1ac)
+val DarkPrimaryBackground = Color(0xff1d2428)
+val DarkSecondaryBackground = Color(0xff14181b)
+
+// Accent Colors
+val LightAccent1 = Color(0x4c4b39ef)
+val LightAccent2 = Color(0x4d39d2c0)
+val LightAccent3 = Color(0x4dee8b60)
+val LightAccent4 = Color(0xccffffff)
+val DarkAccent4 = Color(0xb2262d34)
+
+
+// Semantic Colors
+val LightSuccess = Color(0xFF249689)
+val LightError = Color(0xffff5963)
+val LightWarning = Color(0xfff9cf58)
+val LightInfo = Color(0xffffffff)
+
+// Custom Colors
+val YLnMnBlue = Color(0xff3d5a80)
+val PowderBlue = Color(0xff98c1d9)
+val LightCyan = Color(0xffe0fbfc)
+val BurntSienna = Color(0xffee6c4d)
+val Gunmetal = Color(0xFF293241)
--- mobile/android/src/com/sergioribera/superclipboard/ui/theme/Theme.kt
+++ mobile/android/src/com/sergioribera/superclipboard/ui/theme/Theme.kt
@@ -16,29 +16,33 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Primary,
+    secondary = Secondary,
+    tertiary = Tertiary,
+    background = DarkPrimaryBackground,
+    onPrimary = DarkPrimaryText,
+    onSecondary = DarkSecondaryText,
+    onTertiary = DarkAlternate,
+    onBackground = DarkSecondaryBackground,
+    error = LightError,
+    surface = LightSuccess,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = Primary,
+    secondary = Secondary,
+    tertiary = Tertiary,
+    background = LightPrimaryBackground,
+    onPrimary = LightPrimaryText,
+    onSecondary = LightSecondaryText,
+    onTertiary = LightAlternate,
+    onBackground = LightSecondaryBackground,
+    error = LightError,
+    surface = LightSuccess,
 )
 
 @Composable
-fun JetpackExampleTheme(
+fun AppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
