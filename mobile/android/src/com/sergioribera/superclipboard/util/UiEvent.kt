4c91ac57f3ebc5d004b125db86a845aa1120c2e5
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2023-08-09 02:52:41 -0400

    feat: create ui events

---
+++ mobile/android/src/com/sergioribera/superclipboard/util/UiEvent.kt
@@ -0,0 +1,10 @@
+package com.sergioribera.superclipboard.util
+
+sealed class UiEvent {
+    object PopBackStack: UiEvent()
+    data class Navigate(val route: String): UiEvent()
+    data class ShowSnackbar(
+        val message: String,
+        val action: String? = null
+    ): UiEvent()
+}
