6fc965773a014915cd6004cf68aad92697821add
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2023-08-09 03:13:20 -0400

    feat: generate db instance from context

--- mobile/android/src/com/sergioribera/superclipboard/data/DeviceDatabase.kt
+++ mobile/android/src/com/sergioribera/superclipboard/data/DeviceDatabase.kt
@@ -1,11 +1,32 @@
 package com.sergioribera.superclipboard.data
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import uniffi.mdns.MDnsDevice
 
-@Database(entities = [MDnsDevice::class],
-    version = 1)
-abstract class DeviceDatabase: RoomDatabase() {
+@Database(
+    entities = [MDnsDevice::class],
+    version = 1
+)
+abstract class DeviceDatabase : RoomDatabase() {
     abstract val dao: DeviceDao
+
+    companion object {
+        private var INSTANCE: DeviceDatabase? = null
+
+        fun getInstance(context: Context): DeviceDatabase {
+            if (INSTANCE == null) {
+                INSTANCE = Room.databaseBuilder(context, DeviceDatabase::class.java, "device_db.db")
+                    .build()
+            }
+
+            return INSTANCE!!
+        }
+
+        fun destroyInstance() {
+            INSTANCE = null
+        }
+    }
 }
